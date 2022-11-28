be08cd0cd9fb014f143495bf42135619e65abffe
Ogurchik00 <nik.skripka.000@gmail.com>
2022-11-28 08:41:18 +0300

    modified the genDiff file

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,4 +4,8 @@ const gendiff = require('commander')
 gendiff
 .description('Compares two configuration files and shows a difference.')
 .version('0.1')
+.option('-f, --format <type>', 'output format')
+.argument('<filepath1>')
+.argument('<filepath2>')
+
 gendiff.parse(process.argv)
